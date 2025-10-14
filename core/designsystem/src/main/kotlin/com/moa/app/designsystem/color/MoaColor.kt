e4cf80f0e9cdbfea927ef0a5556db180f14132a7
wjdrjs00 <kimjk3409@gmail.com>
2025-10-15 01:39:43 +0900

    feat: 모아 디자인 컬러시스템 정의

---
+++ core/designsystem/src/main/kotlin/com/moa/app/designsystem/color/MoaColor.kt
@@ -0,0 +1,371 @@
+package com.moa.app.designsystem.color
+
+import androidx.compose.runtime.Immutable
+import androidx.compose.runtime.staticCompositionLocalOf
+import androidx.compose.ui.graphics.Color
+
+internal val Black = Color(0xFF000000)
+internal val White = Color(0xFFFFFFFF)
+
+// Cool Gray Scale
+internal val CoolGray5 = Color(0xFF0F0F10)
+internal val CoolGray7 = Color(0xFF141415)
+internal val CoolGray10 = Color(0xFF171719)
+internal val CoolGray20 = Color(0xFF292A2D)
+internal val CoolGray30 = Color(0xFF46474C)
+internal val CoolGray40 = Color(0xFF5A5C63)
+internal val CoolGray50 = Color(0xFF70737C)
+internal val CoolGray60 = Color(0xFF878A93)
+internal val CoolGray70 = Color(0xFF989BA2)
+internal val CoolGray80 = Color(0xFFAEB0B6)
+internal val CoolGray90 = Color(0xFFC2C4C8)
+internal val CoolGray95 = Color(0xFFDBDCDF)
+internal val CoolGray96 = Color(0xFFE1E2E4)
+internal val CoolGray97 = Color(0xFFEAEBEC)
+internal val CoolGray98 = Color(0xFFF4F4F5)
+internal val CoolGray99 = Color(0xFFF7F7F8)
+
+// Neutral Gray Scale
+internal val NeutralGray5 = Color(0xFF0F0F0F)
+internal val NeutralGray10 = Color(0xFF171717)
+internal val NeutralGray15 = Color(0xFF1C1C1C)
+internal val NeutralGray20 = Color(0xFF2A2A2A)
+internal val NeutralGray22 = Color(0xFF303030)
+internal val NeutralGray30 = Color(0xFF474747)
+internal val NeutralGray40 = Color(0xFF5C5C5C)
+internal val NeutralGray50 = Color(0xFF737373)
+internal val NeutralGray60 = Color(0xFF8A8A8A)
+internal val NeutralGray70 = Color(0xFF9B9B9B)
+internal val NeutralGray80 = Color(0xFFB0B0B0)
+internal val NeutralGray90 = Color(0xFFC4C4C4)
+internal val NeutralGray95 = Color(0xFFDCDCDC)
+internal val NeutralGray99 = Color(0xFFF7F7F7)
+
+// Green Scale
+internal val Green50 = Color(0xFFEDFAE6)
+internal val Green100 = Color(0xFFC6EFB1)
+internal val Green200 = Color(0xFFABE78B)
+internal val Green300 = Color(0xFF84DB55)
+internal val Green400 = Color(0xFF6DD535)
+internal val Green500 = Color(0xFF48CA02)
+internal val Green600 = Color(0xFF42B802)
+internal val Green700 = Color(0xFF338F01)
+internal val Green800 = Color(0xFF286F01)
+internal val Green900 = Color(0xFF1E5501)
+
+// Light Blue Scale
+internal val LightBlue50 = Color(0xFFEFF9FF)
+internal val LightBlue100 = Color(0xFFCFECFF)
+internal val LightBlue200 = Color(0xFFB7E2FF)
+internal val LightBlue300 = Color(0xFF96D5FF)
+internal val LightBlue400 = Color(0xFF82CDFF)
+internal val LightBlue500 = Color(0xFF63C1FF)
+internal val LightBlue600 = Color(0xFF5AB0E8)
+internal val LightBlue700 = Color(0xFF4689B5)
+internal val LightBlue800 = Color(0xFF366A8C)
+internal val LightBlue900 = Color(0xFF2A516B)
+
+// Blue Scale
+internal val Blue50 = Color(0xFFE6EFFF)
+internal val Blue100 = Color(0xFFB0CEFF)
+internal val Blue200 = Color(0xFF8AB7FF)
+internal val Blue300 = Color(0xFF5496FF)
+internal val Blue400 = Color(0xFF3381FF)
+internal val Blue500 = Color(0xFF0062FF)
+internal val Blue600 = Color(0xFF0059E8)
+internal val Blue700 = Color(0xFF0046B5)
+internal val Blue800 = Color(0xFF00368C)
+internal val Blue900 = Color(0xFF00296B)
+
+// Orange Scale
+internal val Orange50 = Color(0xFFFFF3EA)
+internal val Orange100 = Color(0xFFFFDABE)
+internal val Orange200 = Color(0xFFFFC99F)
+internal val Orange300 = Color(0xFFFFB073)
+internal val Orange400 = Color(0xFFFFA158)
+internal val Orange500 = Color(0xFFFF892E)
+internal val Orange600 = Color(0xFFE87D2A)
+internal val Orange700 = Color(0xFFB56121)
+internal val Orange800 = Color(0xFF8C4B19)
+internal val Orange900 = Color(0xFF6B3A13)
+
+// Yellow Scale
+internal val Yellow50 = Color(0xFFFFFAE9)
+internal val Yellow100 = Color(0xFFFFF0BA)
+internal val Yellow200 = Color(0xFFFFE898)
+internal val Yellow300 = Color(0xFFFFDE69)
+internal val Yellow400 = Color(0xFFFFD84C)
+internal val Yellow500 = Color(0xFFFFCE1F)
+internal val Yellow600 = Color(0xFFE8BB1C)
+internal val Yellow700 = Color(0xFFB59216)
+internal val Yellow800 = Color(0xFF8C7111)
+internal val Yellow900 = Color(0xFF6B570D)
+
+// Red Scale
+internal val Red50 = Color(0xFFFFEDED)
+internal val Red100 = Color(0xFFFFC6C6)
+internal val Red200 = Color(0xFFFFAAAA)
+internal val Red300 = Color(0xFFFF8484)
+internal val Red400 = Color(0xFFFF6C6C)
+internal val Red500 = Color(0xFFFF4747)
+internal val Red600 = Color(0xFFE84141)
+internal val Red700 = Color(0xFFB53232)
+internal val Red800 = Color(0xFF8C2727)
+internal val Red900 = Color(0xFF6B1E1E)
+
+// Purple Scale
+internal val Purple50 = Color(0xFFF3F1FF)
+internal val Purple100 = Color(0xFFD9D4FF)
+internal val Purple200 = Color(0xFFC7C0FF)
+internal val Purple300 = Color(0xFFADA3FF)
+internal val Purple400 = Color(0xFF9D91FF)
+internal val Purple500 = Color(0xFF8575FF)
+internal val Purple600 = Color(0xFF796AE8)
+internal val Purple700 = Color(0xFF5E53B5)
+internal val Purple800 = Color(0xFF49408C)
+internal val Purple900 = Color(0xFF38316B)
+
+// Pink Scale
+internal val Pink50 = Color(0xFFFFF6F8)
+internal val Pink100 = Color(0xFFFFE4EA)
+internal val Pink200 = Color(0xFFFFD7DF)
+internal val Pink300 = Color(0xFFFFC5D1)
+internal val Pink400 = Color(0xFFFFB9C8)
+internal val Pink500 = Color(0xFFFFA8BA)
+internal val Pink600 = Color(0xFFE899A9)
+internal val Pink700 = Color(0xFFB57784)
+internal val Pink800 = Color(0xFF8C5C66)
+internal val Pink900 = Color(0xFF6B474E)
+
+@Immutable
+data class MoaColors(
+    val white: Color,
+    val black: Color,
+    val coolGray5: Color,
+    val coolGray7: Color,
+    val coolGray10: Color,
+    val coolGray20: Color,
+    val coolGray30: Color,
+    val coolGray40: Color,
+    val coolGray50: Color,
+    val coolGray60: Color,
+    val coolGray70: Color,
+    val coolGray80: Color,
+    val coolGray90: Color,
+    val coolGray95: Color,
+    val coolGray96: Color,
+    val coolGray97: Color,
+    val coolGray98: Color,
+    val coolGray99: Color,
+    val neutralGray5: Color,
+    val neutralGray10: Color,
+    val neutralGray15: Color,
+    val neutralGray20: Color,
+    val neutralGray22: Color,
+    val neutralGray30: Color,
+    val neutralGray40: Color,
+    val neutralGray50: Color,
+    val neutralGray60: Color,
+    val neutralGray70: Color,
+    val neutralGray80: Color,
+    val neutralGray90: Color,
+    val neutralGray95: Color,
+    val neutralGray99: Color,
+    val green50: Color,
+    val green100: Color,
+    val green200: Color,
+    val green300: Color,
+    val green400: Color,
+    val green500: Color,
+    val green600: Color,
+    val green700: Color,
+    val green800: Color,
+    val green900: Color,
+    val lightBlue50: Color,
+    val lightBlue100: Color,
+    val lightBlue200: Color,
+    val lightBlue300: Color,
+    val lightBlue400: Color,
+    val lightBlue500: Color,
+    val lightBlue600: Color,
+    val lightBlue700: Color,
+    val lightBlue800: Color,
+    val lightBlue900: Color,
+    val blue50: Color,
+    val blue100: Color,
+    val blue200: Color,
+    val blue300: Color,
+    val blue400: Color,
+    val blue500: Color,
+    val blue600: Color,
+    val blue700: Color,
+    val blue800: Color,
+    val blue900: Color,
+    val orange50: Color,
+    val orange100: Color,
+    val orange200: Color,
+    val orange300: Color,
+    val orange400: Color,
+    val orange500: Color,
+    val orange600: Color,
+    val orange700: Color,
+    val orange800: Color,
+    val orange900: Color,
+    val yellow50: Color,
+    val yellow100: Color,
+    val yellow200: Color,
+    val yellow300: Color,
+    val yellow400: Color,
+    val yellow500: Color,
+    val yellow600: Color,
+    val yellow700: Color,
+    val yellow800: Color,
+    val yellow900: Color,
+    val red50: Color,
+    val red100: Color,
+    val red200: Color,
+    val red300: Color,
+    val red400: Color,
+    val red500: Color,
+    val red600: Color,
+    val red700: Color,
+    val red800: Color,
+    val red900: Color,
+    val purple50: Color,
+    val purple100: Color,
+    val purple200: Color,
+    val purple300: Color,
+    val purple400: Color,
+    val purple500: Color,
+    val purple600: Color,
+    val purple700: Color,
+    val purple800: Color,
+    val purple900: Color,
+    val pink50: Color,
+    val pink100: Color,
+    val pink200: Color,
+    val pink300: Color,
+    val pink400: Color,
+    val pink500: Color,
+    val pink600: Color,
+    val pink700: Color,
+    val pink800: Color,
+    val pink900: Color,
+)
+
+val defaultMoaColors = MoaColors(
+    white = White,
+    black = Black,
+    coolGray5 = CoolGray5,
+    coolGray7 = CoolGray7,
+    coolGray10 = CoolGray10,
+    coolGray20 = CoolGray20,
+    coolGray30 = CoolGray30,
+    coolGray40 = CoolGray40,
+    coolGray50 = CoolGray50,
+    coolGray60 = CoolGray60,
+    coolGray70 = CoolGray70,
+    coolGray80 = CoolGray80,
+    coolGray90 = CoolGray90,
+    coolGray95 = CoolGray95,
+    coolGray96 = CoolGray96,
+    coolGray97 = CoolGray97,
+    coolGray98 = CoolGray98,
+    coolGray99 = CoolGray99,
+    neutralGray5 = NeutralGray5,
+    neutralGray10 = NeutralGray10,
+    neutralGray15 = NeutralGray15,
+    neutralGray20 = NeutralGray20,
+    neutralGray22 = NeutralGray22,
+    neutralGray30 = NeutralGray30,
+    neutralGray40 = NeutralGray40,
+    neutralGray50 = NeutralGray50,
+    neutralGray60 = NeutralGray60,
+    neutralGray70 = NeutralGray70,
+    neutralGray80 = NeutralGray80,
+    neutralGray90 = NeutralGray90,
+    neutralGray95 = NeutralGray95,
+    neutralGray99 = NeutralGray99,
+    green50 = Green50,
+    green100 = Green100,
+    green200 = Green200,
+    green300 = Green300,
+    green400 = Green400,
+    green500 = Green500,
+    green600 = Green600,
+    green700 = Green700,
+    green800 = Green800,
+    green900 = Green900,
+    lightBlue50 = LightBlue50,
+    lightBlue100 = LightBlue100,
+    lightBlue200 = LightBlue200,
+    lightBlue300 = LightBlue300,
+    lightBlue400 = LightBlue400,
+    lightBlue500 = LightBlue500,
+    lightBlue600 = LightBlue600,
+    lightBlue700 = LightBlue700,
+    lightBlue800 = LightBlue800,
+    lightBlue900 = LightBlue900,
+    blue50 = Blue50,
+    blue100 = Blue100,
+    blue200 = Blue200,
+    blue300 = Blue300,
+    blue400 = Blue400,
+    blue500 = Blue500,
+    blue600 = Blue600,
+    blue700 = Blue700,
+    blue800 = Blue800,
+    blue900 = Blue900,
+    orange50 = Orange50,
+    orange100 = Orange100,
+    orange200 = Orange200,
+    orange300 = Orange300,
+    orange400 = Orange400,
+    orange500 = Orange500,
+    orange600 = Orange600,
+    orange700 = Orange700,
+    orange800 = Orange800,
+    orange900 = Orange900,
+    yellow50 = Yellow50,
+    yellow100 = Yellow100,
+    yellow200 = Yellow200,
+    yellow300 = Yellow300,
+    yellow400 = Yellow400,
+    yellow500 = Yellow500,
+    yellow600 = Yellow600,
+    yellow700 = Yellow700,
+    yellow800 = Yellow800,
+    yellow900 = Yellow900,
+    red50 = Red50,
+    red100 = Red100,
+    red200 = Red200,
+    red300 = Red300,
+    red400 = Red400,
+    red500 = Red500,
+    red600 = Red600,
+    red700 = Red700,
+    red800 = Red800,
+    red900 = Red900,
+    purple50 = Purple50,
+    purple100 = Purple100,
+    purple200 = Purple200,
+    purple300 = Purple300,
+    purple400 = Purple400,
+    purple500 = Purple500,
+    purple600 = Purple600,
+    purple700 = Purple700,
+    purple800 = Purple800,
+    purple900 = Purple900,
+    pink50 = Pink50,
+    pink100 = Pink100,
+    pink200 = Pink200,
+    pink300 = Pink300,
+    pink400 = Pink400,
+    pink500 = Pink500,
+    pink600 = Pink600,
+    pink700 = Pink700,
+    pink800 = Pink800,
+    pink900 = Pink900
+)
+
+internal val LocalMoaColors = staticCompositionLocalOf { defaultMoaColors }
