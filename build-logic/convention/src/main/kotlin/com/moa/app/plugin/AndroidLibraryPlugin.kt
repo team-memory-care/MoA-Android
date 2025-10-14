a652ae16b0d10c50ec24b2a6fa4ce60c021aad0c
wjdrjs00 <kimjk3409@gmail.com>
2025-10-14 19:15:02 +0900

    chore: 중복 호출 제거

--- build-logic/convention/src/main/kotlin/com/moa/app/plugin/AndroidLibraryPlugin.kt
+++ build-logic/convention/src/main/kotlin/com/moa/app/plugin/AndroidLibraryPlugin.kt
@@ -6,8 +6,8 @@ import org.gradle.api.Project
 class AndroidLibraryPlugin : AndroidBasePlugin() {
     override fun apply(target: Project): Unit = with(target) {
         pluginManager.apply {
-            plugins.apply("com.android.library")
-            plugins.apply("org.jetbrains.kotlin.android")
+            apply("com.android.library")
+            apply("org.jetbrains.kotlin.android")
         }
 
         configureAndroidBase()
