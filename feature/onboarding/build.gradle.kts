db5b3965904fb9b370ed2785e8a68f63c7039033
wjdrjs00 <kimjk3409@gmail.com>
2025-10-21 00:32:33 +0900

    feat: 인적사항 입력 ui 구현

--- feature/onboarding/build.gradle.kts
+++ feature/onboarding/build.gradle.kts
@@ -1,38 +1,15 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.moa.android.library)
+    alias(libs.plugins.moa.android.compose)
+    alias(libs.plugins.moa.android.hilt)
 }
 
 android {
     namespace = "com.moa.app.feature.onboarding"
-    compileSdk = 36
-
-    defaultConfig {
-        minSdk = 28
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
+    implementation(projects.core.designsystem)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
