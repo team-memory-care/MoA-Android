6c67399ebe5d4ec13aaf11808a3fd4740410449a
wjdrjs00 <kimjk3409@gmail.com>
2025-10-22 20:24:24 +0900

    feat: appRoute 정의

---
+++ core/navigation/src/main/java/com/moa/app/navigation/AppRoute.kt
@@ -0,0 +1,18 @@
+package com.moa.app.navigation
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+sealed interface AppRoute {
+    @Serializable
+    data object Splash : AppRoute
+
+    @Serializable
+    data object SignIn : AppRoute
+
+    @Serializable
+    data object SignUpProfile : AppRoute
+
+    @Serializable
+    data object SignUpPhoneAuth : AppRoute
+}
