8933339cc2866424c605e8e6fbdee0d4f00514b2
wjdrjs00 <kimjk3409@gmail.com>
2025-10-22 20:51:01 +0900

    feat: MainViewModel 구현

---
+++ app/src/main/kotlin/com/moa/app/main/MainViewModel.kt
@@ -0,0 +1,14 @@
+package com.moa.app.main
+
+import androidx.lifecycle.ViewModel
+import com.moa.app.navigation.Navigator
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class MainViewModel @Inject constructor(
+    private val navigator: Navigator
+) : ViewModel() {
+
+    val navigationEvents = navigator.events
+}
