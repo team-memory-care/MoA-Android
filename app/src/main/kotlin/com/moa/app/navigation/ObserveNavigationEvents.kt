93603987cc22c91cc07a8cb3d3ddfcd14f6e7e4e
wjdrjs00 <kimjk3409@gmail.com>
2025-10-22 20:52:44 +0900

    feat: navigationEvents 구독을 위한 ObserveNavigationEvents 추가

---
+++ app/src/main/kotlin/com/moa/app/navigation/ObserveNavigationEvents.kt
@@ -0,0 +1,45 @@
+package com.moa.app.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.compose.LocalLifecycleOwner
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.navigation.NavController
+import com.moa.app.main.MainViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+internal fun ObserveNavigationEvents(
+    viewModel: MainViewModel,
+    navController: NavController
+) {
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    DisposableEffect(lifecycleOwner, navController) {
+        val job = lifecycleOwner.lifecycleScope.launch {
+            lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.navigationEvents.collect { event ->
+                    when (event) {
+                        is NavigationEvent.NavigateBack -> navController.popBackStack()
+                        is NavigationEvent.Navigate -> {
+                            navController.navigate(event.route) {
+                                event.options.popUpTo?.let { popUpToRoute ->
+                                    popUpTo(popUpToRoute) {
+                                        inclusive = event.options.inclusive
+                                        saveState = event.options.saveState
+                                    }
+                                }
+                                launchSingleTop = event.options.launchSingleTop
+                                restoreState = event.options.restoreState
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        onDispose { job.cancel() }
+    }
+}
