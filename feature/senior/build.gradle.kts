ddbffc8aade3a0f9fe780c0cf36d3f9e1008147e
wjdrjs00 <kimjk3409@gmail.com>
2025-10-24 03:27:27 +0900

    chore: 컨벤션 플러그인 적용

--- feature/senior/build.gradle.kts
+++ feature/senior/build.gradle.kts
@@ -1,38 +1,16 @@
 plugins {
-    alias(libs.plugins.android.library)
-    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.moa.android.library)
+    alias(libs.plugins.moa.android.compose)
+    alias(libs.plugins.moa.android.hilt)
 }
 
 android {
     namespace = "com.moa.app.feature.senior"
-    compileSdk = 36
-
-    defaultConfig {
-        minSdk = 28
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
+    implementation(projects.core.designsystem)
+    implementation(projects.core.navigation)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
