77a7bcba74b5e7dd18ddbed305df2e8c44ed8051
wjdrjs00 <kimjk3409@gmail.com>
2025-11-23 23:38:26 +0900

    feat: 유저 역할 선택 화면 비즈니스 로직 구현

---
+++ domain/src/main/kotlin/com/moa/app/domain/auth/model/UserRole.kt
@@ -0,0 +1,9 @@
+package com.moa.app.domain.auth.model
+
+/**
+ * 유저 역할 Role
+ *
+ * PARENT : 본인
+ * CHILD : 보호자
+ */
+enum class UserRole { PARENT, CHILD }
--- feature/onboarding/src/main/kotlin/com/moa/app/feature/onboarding/role/SelectUserRoleViewModel.kt
+++ feature/onboarding/src/main/kotlin/com/moa/app/feature/onboarding/role/SelectUserRoleViewModel.kt
@@ -1,16 +1,31 @@
 package com.moa.app.feature.onboarding.role
 
 import androidx.lifecycle.ViewModel
+import com.moa.app.domain.auth.model.UserRole
+import com.moa.app.feature.onboarding.role.model.SelectUserUiState
 import com.moa.app.navigation.AppRoute
 import com.moa.app.navigation.NavigationOptions
 import com.moa.app.navigation.Navigator
 import dagger.hilt.android.lifecycle.HiltViewModel
 import jakarta.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 @HiltViewModel
 class SelectUserRoleViewModel @Inject constructor(
     private val navigator: Navigator
 ) : ViewModel() {
+
+    private val _uiState: MutableStateFlow<SelectUserUiState> = MutableStateFlow(SelectUserUiState.INIT)
+    val uiState: StateFlow<SelectUserUiState> = _uiState.asStateFlow()
+
+    fun updateUserRole(role: UserRole) {
+        _uiState.update { it.copy(userRole = role) }
+    }
+
+    fun navigateToBack() = navigator.navigateBack()
 
     fun navigateToUserConnection() {
         navigator.navigate(
