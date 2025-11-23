d5f87222a6e7069364e4207d5aa28e3a4943a7cb
wjdrjs00 <kimjk3409@gmail.com>
2025-11-24 01:19:52 +0900

    feat: 보호자(자녀) 역할 선택 시 토스트 메시지 표시

--- feature/onboarding/src/main/kotlin/com/moa/app/feature/onboarding/role/SelectUserRoleScreen.kt
+++ feature/onboarding/src/main/kotlin/com/moa/app/feature/onboarding/role/SelectUserRoleScreen.kt
@@ -1,5 +1,6 @@
 package com.moa.app.feature.onboarding.role
 
+import android.widget.Toast
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
@@ -10,21 +11,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.LocalLifecycleOwner
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.flowWithLifecycle
 import com.moa.app.designsystem.R
 import com.moa.app.designsystem.component.core.button.MaButton
 import com.moa.app.designsystem.component.core.button.MaSelectButton
 import com.moa.app.designsystem.component.product.topbar.MaTopAppBar
 import com.moa.app.designsystem.theme.MoaTheme
 import com.moa.app.domain.auth.model.UserRole
+import com.moa.app.feature.onboarding.role.model.SelectUserSideEffect
 import com.moa.app.feature.onboarding.role.model.SelectUserUiState
 
 @Composable
@@ -32,6 +38,21 @@ fun SelectUserRoleScreen(
     viewModel: SelectUserRoleViewModel = hiltViewModel()
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+    val lifecycleOwner = LocalLifecycleOwner.current
+    val context = LocalContext.current
+
+
+    LaunchedEffect(Unit) {
+        viewModel.sideEffect
+            .flowWithLifecycle(lifecycleOwner.lifecycle)
+            .collect { sideEffect ->
+                when (sideEffect) {
+                    is SelectUserSideEffect.ShowToast -> {
+                        Toast.makeText(context, sideEffect.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+    }
 
     SelectUserRoleScreenContent(
         uiState = uiState,
