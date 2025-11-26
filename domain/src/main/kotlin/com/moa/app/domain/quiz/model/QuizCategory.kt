13b31f06ab0cfe16648bf537e35124fe187a03d9
wjdrjs00 <kimjk3409@gmail.com>
2025-11-26 20:08:20 +0900

    feat: QuizCategory 도메인 모델 추가

---
+++ domain/src/main/kotlin/com/moa/app/domain/quiz/model/QuizCategory.kt
@@ -0,0 +1,19 @@
+package com.moa.app.domain.quiz.model
+
+/**
+ * MMSE 기반 퀴즈 카테고리
+ *
+ * PERSISTENCE: 지남력
+ * LINGUISTIC: 언어능력
+ * MEMORY: 기억력
+ * ATTENTION: 주의력
+ * SPACETIME: 시공간
+ */
+enum class QuizCategory {
+    PERSISTENCE,
+    LINGUISTIC,
+    MEMORY,
+    ATTENTION,
+    SPACETIME
+    ;
+}
