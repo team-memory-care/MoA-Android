807955b2950d0c64366d2f530332f31901fa2ea7
wjdrjs00 <kimjk3409@gmail.com>
2025-12-17 23:38:42 +0900

    chore: 린트 수정

--- domain/src/main/kotlin/com/moa/app/domain/quiz/usecase/UploadQuizScoreUseCase.kt
+++ domain/src/main/kotlin/com/moa/app/domain/quiz/usecase/UploadQuizScoreUseCase.kt
@@ -5,7 +5,7 @@ import com.moa.app.domain.quiz.repository.QuizRepository
 import javax.inject.Inject
 
 class UploadQuizScoreUseCase @Inject constructor(
-    private val quizRepository: QuizRepository
+    private val quizRepository: QuizRepository,
 ) {
     suspend operator fun invoke(quizScore: QuizScore): Result<Unit> {
         return quizRepository.uploadQuizScore(quizScore)
