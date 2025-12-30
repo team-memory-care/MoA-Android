e7c7d0fa063b5ae384ad9ea40c9aca191165297a
wjdrjs00 <kimjk3409@gmail.com>
2025-12-31 02:37:00 +0900

    feat: CommonSideAnimation 컴포넌트 구현

--- feature/senior/src/main/kotlin/com/moa/app/feature/senior/quiz/component/QuizSlideAnimation.kt
+++ feature/senior/src/main/kotlin/com/moa/app/feature/senior/quiz/component/QuizSlideAnimation.kt
@@ -5,12 +5,11 @@ import androidx.compose.animation.AnimatedContentTransitionScope
 import androidx.compose.animation.SizeTransform
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.togetherWith
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.moa.app.domain.quiz.model.Quiz
-
-private const val QUIZ_SLIDE_ANIMATION = "QuizSlideAnimation"
 
 @Composable
 fun <T : Quiz> QuizSlideAnimation(
@@ -38,3 +37,36 @@ fun <T : Quiz> QuizSlideAnimation(
         content(state)
     }
 }
+
+@Composable
+fun <T> CommonSideAnimation(
+    targetState: T,
+    modifier: Modifier = Modifier,
+    contentKey: (T) -> Any = { it as Any },
+    content: @Composable (T) -> Unit,
+) {
+    AnimatedContent(
+        targetState = targetState,
+        label = COMMON_SIDE_ANIMATION,
+        contentKey = contentKey,
+        transitionSpec = {
+            (slideIntoContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Start,
+                animationSpec = tween(durationMillis = 300)
+            ) togetherWith slideOutOfContainer(
+                towards = AnimatedContentTransitionScope.SlideDirection.Start,
+                animationSpec = tween(durationMillis = 300)
+            )).using(
+                sizeTransform = SizeTransform { _, _ ->
+                    tween(durationMillis = 0)
+                }
+            )
+        },
+        modifier = modifier
+    ) { state ->
+        content(state)
+    }
+}
+
+private const val COMMON_SIDE_ANIMATION = "CommonSideAnimation"
+private const val QUIZ_SLIDE_ANIMATION = "QuizSlideAnimation"
