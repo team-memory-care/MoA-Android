de558465ce462cd85bfad41afd72c1b11050445c
wjdrjs00 <kimjk3409@gmail.com>
2026-01-12 11:56:12 +0900

    feat: Foldable 디바이스 preview 어노테이션 추가

---
+++ core/ui/src/main/java/com/moa/app/ui/preview/FoldablePreviews.kt
@@ -0,0 +1,15 @@
+package com.moa.app.ui.preview
+
+import androidx.compose.ui.tooling.preview.Preview
+
+@Preview(
+    name = "Z Fold 6 - Main (Unfolded)",
+    device = "spec:width=795dp,height=925dp,dpi=374",
+    showBackground = true
+)
+@Preview(
+    name = "Z Fold 6 - Cover (Folded)",
+    device = "spec:width=378dp,height=927dp,dpi=410",
+    showBackground = true
+)
+annotation class FoldablePreviews
