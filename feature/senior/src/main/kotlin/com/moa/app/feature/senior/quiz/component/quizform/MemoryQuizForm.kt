5f7f75c3a2b3e9d4aa7415d7e8a953362a627e11
wjdrjs00 <kimjk3409@gmail.com>
2026-01-13 01:46:37 +0900

    feat: 오늘의 퀴즈 tts 기능 추가

--- feature/senior/src/main/kotlin/com/moa/app/feature/senior/quiz/component/quizform/MemoryQuizForm.kt
+++ feature/senior/src/main/kotlin/com/moa/app/feature/senior/quiz/component/quizform/MemoryQuizForm.kt
@@ -1,10 +1,7 @@
 package com.moa.app.feature.senior.quiz.component.quizform
 
-import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import com.moa.app.feature.senior.quiz.memory.InputMode
 import com.moa.app.feature.senior.quiz.memory.MemoryQuizSetState
 import com.moa.app.feature.senior.quiz.memory.component.MemoryQuizPlayContent
@@ -20,8 +17,10 @@ fun MemoryQuizForm(
     isChangeModeButtonEnabled: Boolean,
     imageUrls: List<String>,
     userTextAnswers: List<String>,
+    isTextContinueButtonEnabled: Boolean,
     onStartQuizClick: () -> Unit,
     onImagesFinished: () -> Unit,
+    onImageClick: () -> Unit,
     onStartSpeakingClick: () -> Unit,
     onUnableToSpeakClick: () -> Unit,
     onChangeModeClick: () -> Unit,
@@ -48,18 +47,17 @@ fun MemoryQuizForm(
                         showChangeModeButton = isChangeModeButtonEnabled,
                         onStartSpeakingClick = onStartSpeakingClick,
                         onUnableToSpeakClick = onUnableToSpeakClick,
-                        onImageClick = {},
+                        onImageClick = onImageClick,
                         onChangeModeClick = onChangeModeClick,
                     )
                 }
 
                 InputMode.TEXT -> {
                     MemoryQuizTextModeContent(
-                        modifier = Modifier.padding(horizontal = 20.dp),
                         answers = userTextAnswers,
-                        isContinueEnabled = true,
+                        isContinueEnabled = isTextContinueButtonEnabled,
                         onTextAnswerChange = onTextAnswerChange,
-                        onImageClick = {},
+                        onImageClick = onImageClick,
                         onContinueClick = onContinueTextClick,
                     )
                 }
@@ -78,8 +76,10 @@ private fun PreviewMemoryQuizForm() {
         isChangeModeButtonEnabled = false,
         imageUrls = listOf("", "", ""),
         userTextAnswers = listOf("", "", ""),
+        isTextContinueButtonEnabled = false,
         onStartQuizClick = {},
         onImagesFinished = {},
+        onImageClick = {},
         onStartSpeakingClick = {},
         onUnableToSpeakClick = {},
         onChangeModeClick = {},
