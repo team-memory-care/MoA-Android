2bf9d72cede5be7590cc4a1e3436cd0a7b01a497
wjdrjs00 <kimjk3409@gmail.com>
2026-02-27 19:08:03 +0900

    refactor: AndroidTtsManager 로직 개선

--- feature/senior/src/main/kotlin/com/moa/app/feature/senior/quiz/tts/TtsManager.kt
+++ feature/senior/src/main/kotlin/com/moa/app/feature/senior/quiz/tts/TtsManager.kt
@@ -1,8 +1,14 @@
 package com.moa.app.feature.senior.quiz.tts
 
+import kotlinx.coroutines.flow.StateFlow
+
 interface TtsManager {
     val isSpeaking: Boolean
+        get() = playbackState.value == PlaybackState.SPEAKING
+
+    val playbackState: StateFlow<PlaybackState>
+
     fun speak(text: String, isFlush: Boolean = true)
+
     fun stop()
-    fun destroy()
 }
