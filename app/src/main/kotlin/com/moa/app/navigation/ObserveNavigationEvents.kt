084a6111c18a67418ef720bb1327be5cec2374b3
wjdrjs00 <kimjk3409@gmail.com>
2026-03-18 20:16:37 +0900

    refactor: ObserveNavigationEvents 로직 개선
    * refactor: DisposableEffect를 LaunchedEffect로 교체하여 탐색 이벤트 수집 로직 간소화
    * chore: 불필요한 lifecycleScope.launch 및 onDispose 취소 로직 제거

--- app/src/main/kotlin/com/moa/app/navigation/ObserveNavigationEvents.kt
+++ app/src/main/kotlin/com/moa/app/navigation/ObserveNavigationEvents.kt
@@ -1,15 +1,13 @@
 package com.moa.app.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.compose.LocalLifecycleOwner
-import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.NavController
 import com.moa.app.main.MainViewModel
-import kotlinx.coroutines.launch
 
 @Composable
 internal fun ObserveNavigationEvents(
@@ -19,36 +17,32 @@ internal fun ObserveNavigationEvents(
     val context = LocalContext.current
     val lifecycleOwner = LocalLifecycleOwner.current
 
-    DisposableEffect(lifecycleOwner, navController) {
-        val job = lifecycleOwner.lifecycleScope.launch {
-            lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
-                viewModel.navigationEvents.collect { event ->
-                    when (event) {
-                        is NavigationEvent.NavigateBack -> navController.popBackStack()
-                        is NavigationEvent.OpenUrl -> openUrlInBrowser(context, event.url)
-                        is NavigationEvent.Navigate -> {
-                            navController.navigate(event.route) {
-                                if (event.options.clearBackStack) {
-                                    popUpTo(navController.graph.id) {
-                                        inclusive = true
-                                    }
-                                } else {
-                                    event.options.popUpTo?.let { popUpToRoute ->
-                                        popUpTo(popUpToRoute) {
-                                            inclusive = event.options.inclusive
-                                            saveState = event.options.saveState
-                                        }
+    LaunchedEffect(viewModel.navigationEvents, lifecycleOwner) {
+        lifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED) {
+            viewModel.navigationEvents.collect { event ->
+                when (event) {
+                    is NavigationEvent.NavigateBack -> navController.popBackStack()
+                    is NavigationEvent.OpenUrl -> openUrlInBrowser(context, event.url)
+                    is NavigationEvent.Navigate -> {
+                        navController.navigate(event.route) {
+                            if (event.options.clearBackStack) {
+                                popUpTo(navController.graph.id) {
+                                    inclusive = true
+                                }
+                            } else {
+                                event.options.popUpTo?.let { popUpToRoute ->
+                                    popUpTo(popUpToRoute) {
+                                        inclusive = event.options.inclusive
+                                        saveState = event.options.saveState
                                     }
                                 }
-                                launchSingleTop = event.options.launchSingleTop
-                                restoreState = event.options.restoreState
                             }
+                            launchSingleTop = event.options.launchSingleTop
+                            restoreState = event.options.restoreState
                         }
                     }
                 }
             }
         }
-
-        onDispose { job.cancel() }
     }
 }
